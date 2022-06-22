4e80e5cf722f5ea13ae6c2b489253a1774b0c9d5
Lorenzo Lopez Uroz <103563685+lolopezuroz@users.noreply.github.com>
2022-06-22 17:22:26 +0200

    no use

--- backup/v1/build_model.py
+++
@@ -1,6 +0,0 @@
-from train import train
-from parameters import args
-from glacier_dataset import load_dataset
-
-train_dataset, test_dataset = load_dataset(args)
-train(args,train_dataset,test_dataset)
