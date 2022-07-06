072912e0f933b8278c56caa76fa5f6c27d4f2448
Lorenzo Lopez Uroz <103563685+lolopezuroz@users.noreply.github.com>
2022-07-06 13:46:47 +0200

    major changes

--- build_model.py
+++
@@ -1,6 +0,0 @@
-from functions.train import train
-from functions.glacier_dataset import load_dataset
-from training_parameters import args
-
-train_dataset, test_dataset = load_dataset(args)
-train(args, train_dataset, test_dataset)
